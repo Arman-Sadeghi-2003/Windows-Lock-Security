e58afed4506f6ddfb97d24a46319f5d7010108d1
Arman Sadeghi <Terminator_arman@yahoo.com>
2024-12-19 17:35:03 +0330

    Add README.md and LICENSE.txt.

---
+++ WindowsLockSecurity/Assets/Readme.md
@@ -0,0 +1,7 @@
+# How to Use
+
+- The application runs automatically at system startup.
+- To close, press the K key.
+- Ensure no keyboard activity for 10 seconds to trigger system shutdown.
+
+## Terminator_arman@yahoo.com
---
+++ WindowsLockSecurity/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace WindowsLockSecurity
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetRegister();
+
+            timer.Interval = TimeSpan.FromSeconds(10);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void SetRegister()
+        {
+            string appName = "Windows Lock Security (AILC)"; // Replace with your app name
+            //string appPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string appPath = Environment.ProcessPath;
+
+            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            registryKey.SetValue(appName, $"\"{appPath}\"");
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("shutdown", "/s /t 0")
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false
+            });
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.K)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
