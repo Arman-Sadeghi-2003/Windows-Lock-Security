fac4a4a4bd61a0201713361f441fccdd28efd30b
Arman Sadeghi <terminator_arman@yahoo.com>
2025-06-15 12:56:35 +0330

    minor

--- WindowsLockSecurity.Settings/MainWindow.xaml.cs
+++ WindowsLockSecurity.Settings/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WindowsLockSecurity.Settings
 {
@@ -16,9 +8,22 @@ namespace WindowsLockSecurity.Settings
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<string> Keys { get; set; } = new();
+
         public MainWindow()
         {
             InitializeComponent();
+            initializeProperties();
+            DataContext = this;
+        }
+
+        private void initializeProperties()
+        {
+            for (char c = 'A'; c <= 'Z'; c++)
+                Keys.Add(c.ToString());
+            for (char c = '0'; c <= '0'; c++)
+                Keys.Add(c.ToString());
+
         }
     }
 }
